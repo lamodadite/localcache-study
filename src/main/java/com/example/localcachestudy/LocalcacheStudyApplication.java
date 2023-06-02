2d6d865625a240a89cd4ab73589aa71e8475af79
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-06-02 13:27:50 +0900

    Initial commit

---
+++ src/main/java/com/example/localcachestudy/LocalcacheStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.localcachestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalcacheStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalcacheStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/localcachestudy/LocalcacheStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localcachestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalcacheStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
